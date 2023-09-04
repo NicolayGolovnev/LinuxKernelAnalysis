1397cf72275d2f1819c39f6832f63c7ff04eca63
Nik <nikstarall@gmail.com>
2023-09-05 01:11:48 +0700

    Рефактор жизенного цикла программы с более явным выделением преобразования данных Удалил вывод графиков В некоторых местах используются глобальные переменные, нужно будет исправить

--- src/main/kotlin/ru/altstu/python_helper/main.py
+++ src/main/kotlin/ru/altstu/python_helper/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import List, Callable
 
 import nltk
 
@@ -35,6 +36,7 @@ def get_metric(tree, count_messages, count_clasters, treshold):
     return sum([index[1] for index in counter[:count_clasters]]) / count_messages * 100, counter[count_clasters - 1][
         1] / counter[0][1] * 100
 
+
 def get_filter_method():
     vectorizer = CommitVectorizer()
     white_samples, grey_samples = vectorizer.make_samples(repo)
@@ -43,14 +45,54 @@ def get_filter_method():
     classifire.fit(white_vectors, gray_vectors)
     return classifire.is_bugfix_commit
 
-def set_dir(name:str) -> None:
-    current_folder = f"{config.result_path}/{analysisData.name}"
-    if not os.path.exists(current_folder):
-        os.mkdir(current_folder)
-    os.chdir(current_folder)
 
-def back_dir() -> None:
-    os.chdir("..")
+class TestFileManager:
+
+    def __init__(self, tests_data: List[DirectoryAnalysisData]):
+        self.__root_dir: str = os.getcwd()
+        self.tests_data: List[DirectoryAnalysisData] = tests_data
+
+    def full_iteration(self, iteration_func: Callable[[DirectoryAnalysisData], None]) -> None:
+        for test_data in self.tests_data:
+            self._set_dir(test_data.name)
+            iteration_func(test_data)
+            self._back_dir()
+
+    def _back_dir(self) -> None:
+        os.chdir(self.__root_dir)
+
+    def _set_dir(self, name: str) -> None:
+        current_folder = f"{config.result_path}/{name}"
+        if not os.path.exists(current_folder):
+            os.mkdir(current_folder)
+        os.chdir(current_folder)
+
+
+def perfome_test(analysisData: DirectoryAnalysisData):
+    result_sample = CommitSample(
+        repo,
+        folder=analysisData.path,
+        max_len=analysisData.max_commit,
+        commit_filter=filter_method
+    )
+
+    tt = TextTokenizer()
+    vectors = tt.sample_to_vectors(result_sample.list)
+    matrix = create_matrix(vectors)
+
+    Z = linkage(matrix, 'single', metric='average')
+
+    tf = TresholdFinder(Z, len(vectors), config.count_clusters_in_sample)
+    threshold = tf.find_treshold()
+
+    labels = fcluster(Z, threshold, criterion='distance')
+    config.save_np(config.labels_path, labels)
+
+def read_input_data_from_file(path:str):
+    with open(f"{config.input_path}/{path}") as json_config:
+        json_data = json.load(json_config)
+    return DirectoryAnalysisData(**json_data)
+
 
 
 if __name__ == '__main__':
@@ -58,58 +100,11 @@ if __name__ == '__main__':
     repo = git.Repo(config.repo_path)
     filter_method = get_filter_method()
 
-    for file_path in os.listdir(config.input_path):
-        with open(f"{config.input_path}/{file_path}") as json_config:
-            json_data = json.load(json_config)
-        analysisData = DirectoryAnalysisData(**json_data)
-        set_dir(analysisData.name)
-
-        result_sample = CommitSample(
-            repo,
-            folder=analysisData.path,
-            max_len=analysisData.max_commit,
-            commit_filter=filter_method
-        )
-
-        tt = TextTokenizer()
-        vectors = tt.sample_to_vectors(result_sample.list)
-        matrix = create_matrix(vectors)
-
-        Z = linkage(matrix, 'single', metric='average')
-
-        tf = TresholdFinder(Z, len(vectors), config.count_clusters_in_sample)
-        threshold = tf.find_treshold()
-
-        labels = fcluster(Z, threshold, criterion='distance')
-        config.save_np(config.labels_path, labels)
+    input_data = [read_input_data_from_file(path) for path in  os.listdir(config.input_path)]
+    tfm = TestFileManager(input_data)
+    tfm.full_iteration(perfome_test)
 
 
 
-        counter = Counter(labels)
-        counter = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-        for index in counter[:config.count_clusters_in_sample]:
-            cluster = cluster_to_string(vectors, labels, index[0])
-            print('[', ', '.join(tt.vector_to_message(cluster)), ']')
 
-        print("Find", len(vectors), "messages")
-        print("Covered", get_metric(Z, len(vectors), config.count_clusters_in_sample, threshold)[0], "%")
-        print("Fragmentation", get_metric(Z, len(vectors), config.count_clusters_in_sample, threshold)[1], "%")
 
-        plt.figure(figsize=(10, 5))
-        plt.title('Hierarchical Clustering Dendrogram')
-        plt.ylabel('Distance')
-        dend = dendrogram(Z, color_threshold=threshold, no_labels=True)
-        plt.axhline(y=threshold, color='r', linestyle='--')
-        plt.show()
-
-        X = np.arange(0, 0.8, 0.001)
-        Y1 = np.array([get_metric(Z, len(vectors), config.count_clusters_in_sample, x)[0] for x in X])
-        Y2 = np.array([get_metric(Z, len(vectors), config.count_clusters_in_sample, x)[1] for x in X])
-
-        plt.figure(figsize=(10, 5))
-        plt.plot(X, Y1, label='Covered')
-        plt.plot(X, Y2, label='Fragmentation')
-        plt.axvline(x=threshold, color='r', linestyle='--')
-
-        plt.legend()
-        plt.show()
